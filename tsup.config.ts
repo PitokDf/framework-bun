69e02fb9f051fd937a7f6b9253dd0d4775221e47
Pito Desri Pauzi <108637369+PitokDf@users.noreply.github.com>
2026-07-11 20:39:02 +0700

    chore: setup tsup build and configure NPM publish workflow

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["src/exports.ts", "src/cli.ts"],
+	format: ["cjs", "esm"],
+	dts: true,
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	target: "node18",
+    outDir: "dist",
+	external: [/^bun:.*/],
+});
